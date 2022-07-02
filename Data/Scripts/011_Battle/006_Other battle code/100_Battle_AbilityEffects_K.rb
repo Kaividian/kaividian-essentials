881d5ddca1ea29712a8184f479a3d8b4dd8f601a
Kaividian <kaividian@gmail.com>
2022-07-02 15:48:04 +1000

    Added Intangible Ability and updated Frosted Ability Description

--- Data/Scripts/011_Battle/006_Other battle code/100_Battle_AbilityEffects_K.rb
+++ Data/Scripts/011_Battle/006_Other battle code/100_Battle_AbilityEffects_K.rb
@@ -3,3 +3,20 @@ Battle::AbilityEffects::OnSwitchIn.add(:FROSTED,
     battler.effects[PBEffects::Type3] = :ICE
   }
 )
+
+Battle::AbilityEffects::MoveImmunity.add(:INTANGIBLE,
+  proc { |ability, user, target, move, type, battle, show_message|
+
+    if (type == :NORMAL || type == :FIGHTING) && !target.effects[PBEffects::Foresight] && !user.hasActiveAbility?(:SCRAPPY)
+      if show_message
+        @battle.pbShowAbilitySplash(target)
+        if Battle::Scene::USE_ABILITY_SPLASH
+          @battle.pbDisplay(_INTL("{1} avoided the attack!", target.pbThis))
+        else
+          @battle.pbDisplay(_INTL("{1} avoided the attack with {2}!", target.pbThis, target.abilityName))
+      end
+      @battle.pbHideAbilitySplash(target)
+    end
+  end
+    }
+)
