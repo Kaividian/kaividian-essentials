ac8f353a6cd366c82ddb074abd9ed948aa33424b
Kaividian <kaividian@gmail.com>
2023-09-27 07:43:17 +1000

    Multisave working. Configurable number of saves

--- Data/Scripts/001_Settings.rb
+++ Data/Scripts/001_Settings.rb
@@ -453,6 +453,11 @@ module Settings
       "Pizza"
     ]
   end
+
+ #=============================================================================
+
+  ALLOWED_SAVE_FILES = 3
+
 end
 
 # DO NOT EDIT THESE!
