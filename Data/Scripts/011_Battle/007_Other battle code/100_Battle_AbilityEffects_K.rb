8f416fb8aa66b99e6378a8d6dcf7fde789b3ee27
Kaividian <kaividian@gmail.com>
2023-11-01 16:10:31 +1100

    Updated Frosted ability to v21

--- Data/Scripts/011_Battle/007_Other battle code/100_Battle_AbilityEffects_K.rb
+++ Data/Scripts/011_Battle/007_Other battle code/100_Battle_AbilityEffects_K.rb
@@ -1,6 +1,6 @@
 Battle::AbilityEffects::OnSwitchIn.add(:FROSTED,
   proc { |ability, battler, battle, switch_in|
-    battler.effects[PBEffects::Type3] = :ICE
+    battler.effects[PBEffects::ExtraType] = :ICE
   }
 )
 
