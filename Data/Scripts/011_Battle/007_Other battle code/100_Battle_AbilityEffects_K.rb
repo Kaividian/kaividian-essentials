b478a460b52c6a04a1ef575bce7ce7b4ca4bdf38
Kaividian <kaividian@gmail.com>
2023-11-14 06:49:16 +1100

    Updated Intangible -Not in Maple- needed updating

--- Data/Scripts/011_Battle/007_Other battle code/100_Battle_AbilityEffects_K.rb
+++ Data/Scripts/011_Battle/007_Other battle code/100_Battle_AbilityEffects_K.rb
@@ -9,13 +9,13 @@ Battle::AbilityEffects::MoveImmunity.add(:INTANGIBLE,
 
     if (type == :NORMAL || type == :FIGHTING) && !target.effects[PBEffects::Foresight] && !user.hasActiveAbility?(:SCRAPPY)
       if show_message
-        @battle.pbShowAbilitySplash(target)
+        battle.pbShowAbilitySplash(target)
         if Battle::Scene::USE_ABILITY_SPLASH
-          @battle.pbDisplay(_INTL("{1} avoided the attack!", target.pbThis))
+          battle.pbDisplay(_INTL("{1} avoided the attack!", target.pbThis))
         else
-          @battle.pbDisplay(_INTL("{1} avoided the attack with {2}!", target.pbThis, target.abilityName))
+          battle.pbDisplay(_INTL("{1} avoided the attack with {2}!", target.pbThis, target.abilityName))
       end
-      @battle.pbHideAbilitySplash(target)
+      battle.pbHideAbilitySplash(target)
     end
   end
     }
